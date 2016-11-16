6720285fc71fe9dbbf6bdc166ae06a5516d97082
domiique d <dominique-77220@hotmail.fr>
2016-11-16 15:30:52 +0100

    d

---
+++ src/data/IReadService.java
@@ -0,0 +1,28 @@
+package data;
+
+import java.util.ArrayList;
+
+import geometrie.Terrain;
+import geometrie.Vecteur;
+
+public interface IReadService {
+	
+	//getter RObotMaid
+	public Vecteur getRobotPosition();
+	public Vecteur getRobotDirection();
+	public String getRobotName();
+	public String getRobotRole();
+	public double getRobotVitesse();
+	//getter AppartImpl
+	public Terrain getTerrain(int i, int j);
+	public Terrain getTerrain(Vecteur v);
+	public Vecteur getPointDepart();
+	public Vecteur getDirectionDepart();
+	public Vecteur getDirectionArrivee();
+	public int getWidth();
+	public int getHeight();
+	public ArrayList<Vecteur> getArrivees();
+	public double getDist(int i, int j);
+	public Terrain [][] getMatrix();
+	public String getName();
+}
---
+++ src/data/IWriteService.java
@@ -0,0 +1,14 @@
+package data;
+
+import geometrie.Vecteur;
+
+public interface IWriteService {
+
+	public Vecteur setRobotPosition(Vecteur position);
+	public Vecteur setRobotDirection(Vecteur direction);
+	public String setRobotName(String name);
+	public String setRobotRole(String role);
+	public double setRobotVitesse(double vitesse);
+	
+	
+}
